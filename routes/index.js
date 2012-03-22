e7b937610c1836b857708d5b3c308f35ff0b9376
karl <karl@debian.rocketinternet.local>
2012-03-22 21:17:25 +0100

    made things independent from fuuuuu.de

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,19 @@
  * GET home page.
  */
 
+function getHostName(host){
+  return host.match(/^([^:]*)/)[1];
+}
+
 exports.index = function(req, res){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
-  res.render('index', { title: 'Express' })
+    
+  var host = getHostName(req.headers.host);
+  var socketPort = 9999;
+    
+  res.render('index', {
+    title: 'Express', 
+    host: host,
+    socketPort: socketPort,
+    socketJavascriptFile: 'http://' + host + ':' + socketPort + '/socket.io/socket.io.js'
+  })
 };
